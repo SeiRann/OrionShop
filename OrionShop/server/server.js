640cac83b732c0c4030ecc3d8df40d6519e26026
SeiRan <bo646ru@gmail.com>
2023-08-29 01:37:41 +0300

    Added GET/POST requests for accounts

--- OrionShop/server/server.js
+++ OrionShop/server/server.js
@@ -1,12 +1,13 @@
 const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
+const bcrypt = require("bcrypt")
 
-const app = express()
+const app = express();
 
-app.use(cors())
+app.use(cors());
 
-mongoose
+mongoose //Connect to mongoDB
     .connect("mongodb://0.0.0.0:27017/shop", {
         useNewUrlParser: true,
         useUnifiedTopology: true,
@@ -42,7 +43,7 @@ const Game = mongoose.model("Game", gameSchema);
 
 app.use(express.json());
 
-app.post("/create", (req,res) => {
+app.post("/create", (req,res) => { // POST request for a GAME
     const { name, desc, developer, publisher, releaseDate, tags, gallery, thumbnail, trailer, price } = req.body;
 
     const newGame = new Game({ name, desc, developer, publisher, releaseDate, tags, gallery, thumbnail, trailer, price });
@@ -57,7 +58,7 @@ app.post("/create", (req,res) => {
         })
 });
 
-app.post("/create/account", (req, res) => {
+app.post("/create/account", (req, res) => { // POST request for an ACCOUNT
     const { username, email, password } = req.body;
 
     const newAccount = new Account({username, email, password})
@@ -72,8 +73,17 @@ app.post("/create/account", (req, res) => {
         })
 })
 
-app.post("/account/login", (req, res) => {
-    const { username, email, password } = req.body;
+app.get("/accounts/:username", (req,res) => { //GET request for an ACCOUNT by ID
+    const { username } = req.params;
+    
+    Account.findById(username)
+        .then((account) => {
+            res.json(account);
+        })
+        .catch((err) => {
+            res.status(500).json({ msg: err});
+        })
+
 })
 
 
@@ -88,7 +98,6 @@ app.get("/games", (req,res) => {
         })
 })
 
-app.get("/accounts")
 
 app.get("/games/:id", (req, res) => {
     const { id } = req.params
